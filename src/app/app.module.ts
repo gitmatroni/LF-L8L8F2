bd0865486e3abe4d8dbd07ac45fedf020bdfeb14
Gustavo Matroni <gustavogmatroni@hotmail.com>
2020-03-25 02:04:50 -0300

    all components and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { TeamComponent } from './team/team.component';
+import { ContentComponent } from './content/content.component';
+import { AdvertiseComponent } from './advertise/advertise.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardsComponent } from './cards/cards.component';
+import { LinksComponent } from './links/links.component';
+import { SearchComponent } from './search/search.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    TeamComponent,
+    ContentComponent,
+    AdvertiseComponent,
+    FooterComponent,
+    CardsComponent,
+    LinksComponent,
+    SearchComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule
